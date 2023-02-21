909d3c98dce16043e747fb98b862194aabd03e8f
Angelfmc <612110393@utsalamanca.edu.mx>
2023-02-21 12:47:53 -0600

    Terminado back

--- index.js
+++ index.js
@@ -80,9 +80,59 @@ app.post('/registro', (req, res) =>{
   console.log('Usuarios', users)
   res.json({
     'alert': 'success',
-    users
+    data
   })
  })
+
+
+ app.get('/usuarios', async (req, res) =>{
+  const colRef = collection(db, 'users')
+  const docsSnap = await getDocs(colRef)
+  let data = []
+  docsSnap.forEach(doc => {
+    data.push(doc.data())
+  })
+  res.json({
+    'alert': 'success',
+    data
+ })
+ })
+
+app.post('/login', (req, res) =>{
+  let { email, password } = req.body
+
+  if(!email.length || !password.length) {
+    return res.json({
+      'alert': 'No se han recibido los datos correctamente'
+    })
+  }
+
+  const users = collection(db, 'users')
+  getDoc(doc(users, email))
+  .then( user => {
+    if(!user.exists()){
+      return res.json({
+        'alert': 'Correo no registrado en la base de datos'
+    })
+    }else {
+      bcrypt.compare(password, user.data().password, (error, result) => {
+        if(result) {
+          let data = user.data()
+          res.json({
+            'alert': 'Success',
+            name: data.name,
+            email: data.email
+          })
+        } else {
+            return res.json({
+              'alert': 'Password Incorrecto'
+            })
+        }
+      })
+    }
+  })
+})
+
 const PORT = process.env.PORT||19000
 
 //ejecutamos el servidor
